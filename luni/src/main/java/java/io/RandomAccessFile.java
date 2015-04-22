73ce95136dff526e3431a44e005a9817d74500a4
Sergio Giro <sgiro@google.com>
2015-04-22 12:07:42 +0100

    java/io/RandomAccessFile: do not nullify FileChannel when closing the RAF
    getChannel() should return always the same FileChannel, even if it's closed
    
    Bug: 19892782
    Change-Id: I5bd853b6b0931d9a6320a655c4ef982c9beaa6c0

--- luni/src/main/java/java/io/RandomAccessFile.java
+++ luni/src/main/java/java/io/RandomAccessFile.java
@@ -150,7 +150,6 @@ public class RandomAccessFile implements DataInput, DataOutput, Closeable {
         synchronized (this) {
             if (channel != null && channel.isOpen()) {
                 channel.close();
-                channel = null;
             }
             IoBridge.closeAndSignalBlockedThreads(fd);
         }
@@ -174,6 +173,10 @@ public class RandomAccessFile implements DataInput, DataOutput, Closeable {
      * as this file's file pointer offset (see {@link #getFilePointer()}). Any
      * changes made to this file's file pointer offset are also visible in the
      * file channel's position and vice versa.
+     *
+     * Closing the channel closes the RandomAccessFile as well. The instance
+     * of FileChannel returned is always the same even if the RandomAccessFile
+     * or the FileChannel have been closed.
      *
      * @return this file's file channel instance.
      */
