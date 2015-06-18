09489e100f54cf117615ec554dd4c763d19a24ea
Erik Kline <ek@google.com>
2015-06-18 18:14:36 +0900

    Add ENONET to OsConstants
    Change-Id: I155f3db741bdac96f1ecb1ccb15b31faab85e819

--- luni/src/main/java/android/system/OsConstants.java
+++ luni/src/main/java/android/system/OsConstants.java
@@ -204,6 +204,7 @@ public final class OsConstants {
     public static final int ENOLINK = placeholder();
     public static final int ENOMEM = placeholder();
     public static final int ENOMSG = placeholder();
+    /** @hide */ public static final int ENONET = placeholder();
     public static final int ENOPROTOOPT = placeholder();
     public static final int ENOSPC = placeholder();
     public static final int ENOSR = placeholder();
@@ -778,6 +779,9 @@ public final class OsConstants {
         }
         if (errno == ENOMSG) {
             return "ENOMSG";
+        }
+        if (errno == ENONET) {
+            return "ENONET";
         }
         if (errno == ENOPROTOOPT) {
             return "ENOPROTOOPT";
