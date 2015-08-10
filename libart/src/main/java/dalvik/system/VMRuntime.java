multi-os-engine/libcore

Move heap trimming daemon and GC deamon into a single daemon
Bug: 18739541
Change-Id: Icf309cd8981372f24caa7c8b98a2e0591b1d90ea

libart/src/main/java/dalvik/system/VMRuntime.java
@@ -295,10 +295,12 @@ public final class VMRuntime {
      */
     public native void registerNativeFree(int bytes);
 
-    public native void trimHeap();
-    public native void concurrentGC();
     public native void requestConcurrentGC();
-    public native void waitForConcurrentGCRequest();
+    public native void concurrentGC();
+    public native void requestHeapTrim();
+    public native void trimHeap();
+    public native void interruptHeapTaskProcessor();
+    public native void runHeapTasks();
 
     /**
      * Let the heap know of the new process state. This can change allocation and garbage collection

jgcaaprom/android_libcore

libart/src/main/java/dalvik/system/VMRuntime.java
@@ -297,10 +297,13 @@ public final class VMRuntime {
      */
     public native void registerNativeFree(int bytes);
 
-    public native void trimHeap();
-    public native void concurrentGC();
     public native void requestConcurrentGC();
-    public native void waitForConcurrentGCRequest();
+    public native void concurrentGC();
+    public native void requestHeapTrim();
+    public native void trimHeap();
+    public native void startHeapTaskProcessor();
+    public native void stopHeapTaskProcessor();
+    public native void runHeapTasks();
 
     /**
      * Let the heap know of the new process state. This can change allocation and garbage collection