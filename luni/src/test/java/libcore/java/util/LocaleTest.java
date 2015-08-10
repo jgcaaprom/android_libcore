multi-os-engine/libcore

Fix LocaleTest#testDefaultLocale.
The implementation considers user.locale / region / language
immutable if set at the command line.

For the purposes of this test, we'll have to resort to changing
the unchangeable by hand. Fortunately, we already have such a
function thanks to the native bridge.

bug: 19329061

Change-Id: I583a0da355503b547c60e2df6da4c23ac5882e4a

luni/src/test/java/libcore/java/util/LocaleTest.java
@@ -1172,18 +1172,18 @@ public class LocaleTest extends junit.framework.TestCase {
         final String userLocale = System.getProperty("user.locale", "");
         try {
             // Assert that user.locale gets priority.
-            System.setProperty("user.locale", "de-DE");
-            System.setProperty("user.language", "en");
-            System.setProperty("user.region", "US");
+            System.initUnchangeableSystemProperty("user.locale", "de-DE");
+            System.initUnchangeableSystemProperty("user.language", "en");
+            System.initUnchangeableSystemProperty("user.region", "US");
 
             Locale l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("de", l.getLanguage());
             assertEquals("DE", l.getCountry());
 
             // Assert that it's parsed as a full language tag.
-            System.setProperty("user.locale", "de-Latn-DE");
-            System.setProperty("user.language", "en");
-            System.setProperty("user.region", "US");
+            System.initUnchangeableSystemProperty("user.locale", "de-Latn-DE");
+            System.initUnchangeableSystemProperty("user.language", "en");
+            System.initUnchangeableSystemProperty("user.region", "US");
 
             l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("de", l.getLanguage());
@@ -1192,15 +1192,15 @@ public class LocaleTest extends junit.framework.TestCase {
 
             // Assert that we use "und" if we're faced with a bad language tag, and
             // that we don't end up with a null default locale or an exception.
-            System.setProperty("user.locale", "dexx-Latn-DE");
+            System.initUnchangeableSystemProperty("user.locale", "dexx-Latn-DE");
 
             l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("und", l.getLanguage());
             assertEquals("DE", l.getCountry());
         } finally {
-            System.setProperty("user.language", userLanguage);
-            System.setProperty("user.region", userRegion);
-            System.setProperty("user.locale", userLocale);
+            System.initUnchangeableSystemProperty("user.language", userLanguage);
+            System.initUnchangeableSystemProperty("user.region", userRegion);
+            System.initUnchangeableSystemProperty("user.locale", userLocale);
         }
     }
 }

jgcaaprom/android_libcore

luni/src/test/java/libcore/java/util/LocaleTest.java
@@ -1172,18 +1172,18 @@ public class LocaleTest extends junit.framework.TestCase {
         final String userLocale = System.getProperty("user.locale", "");
         try {
             // Assert that user.locale gets priority.
-            System.setProperty("user.locale", "de-DE");
-            System.setProperty("user.language", "en");
-            System.setProperty("user.region", "US");
+            System.setUnchangeableSystemProperty("user.locale", "de-DE");
+            System.setUnchangeableSystemProperty("user.language", "en");
+            System.setUnchangeableSystemProperty("user.region", "US");
 
             Locale l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("de", l.getLanguage());
             assertEquals("DE", l.getCountry());
 
             // Assert that it's parsed as a full language tag.
-            System.setProperty("user.locale", "de-Latn-DE");
-            System.setProperty("user.language", "en");
-            System.setProperty("user.region", "US");
+            System.setUnchangeableSystemProperty("user.locale", "de-Latn-DE");
+            System.setUnchangeableSystemProperty("user.language", "en");
+            System.setUnchangeableSystemProperty("user.region", "US");
 
             l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("de", l.getLanguage());
@@ -1192,15 +1192,15 @@ public class LocaleTest extends junit.framework.TestCase {
 
             // Assert that we use "und" if we're faced with a bad language tag, and
             // that we don't end up with a null default locale or an exception.
-            System.setProperty("user.locale", "dexx-Latn-DE");
+            System.setUnchangeableSystemProperty("user.locale", "dexx-Latn-DE");
 
             l = Locale.getDefaultLocaleFromSystemProperties();
             assertEquals("und", l.getLanguage());
             assertEquals("DE", l.getCountry());
         } finally {
-            System.setProperty("user.language", userLanguage);
-            System.setProperty("user.region", userRegion);
-            System.setProperty("user.locale", userLocale);
+            System.setUnchangeableSystemProperty("user.language", userLanguage);
+            System.setUnchangeableSystemProperty("user.region", userRegion);
+            System.setUnchangeableSystemProperty("user.locale", userLocale);
         }
     }
 }